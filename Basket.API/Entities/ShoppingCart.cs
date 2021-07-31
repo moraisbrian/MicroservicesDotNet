7e8cf4c9ad5cba3561893e774657e00d9dbb0e6b
Brian Leonardo de Morais <morais.brian360@gmail.com>
2021-07-31 10:31:31 -0300

    Adicionado Dockerfile e Docker Compose da API Basket

--- Basket.API/Entities/ShoppingCart.cs
+++ Basket.API/Entities/ShoppingCart.cs
@@ -12,12 +12,13 @@ namespace Basket.API.Entities
         public ShoppingCart(string userName)
         {
             UserName = userName;
+            Items = new List<ShoppingCartItem>();
         }
 
         public string UserName { get; set; }
 
         public List<ShoppingCartItem> Items { get; set; }
 
-        public decimal TotalPrice => Items.Sum(x => x.Quantity * x.Price);
+        public decimal TotalPrice => Items.Sum(item => item.Quantity * item.Price);
     }
 }
